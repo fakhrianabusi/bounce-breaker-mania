62264cc5c92668e2b3190185fc365b836aacbe6d
tvlas <thiago.gilford@gmail.com>
2024-04-28 18:59:38 -0300

    feat: player stick controller

--- lib/configuration/constants.dart
+++ lib/configuration/constants.dart
@@ -3,3 +3,6 @@ import 'package:flutter/material.dart';
 const screenWidth = 720.00;
 const screenHeight = 1600.00;
 const screenColor = Color(0xFF22272B);
+const playerStickWidth = 200.00;
+const playerStickHeight = 40.00;
+const playerStickMoveSteps = screenWidth * 0.03;
---
+++ lib/game_objects/player_stick.dart
@@ -0,0 +1,10 @@
+import 'package:bounce_breaker/main.dart';
+import 'package:flame/components.dart';
+import 'package:flame/events.dart';
+
+class PlayerStick extends PositionComponent
+    with DragCallbacks, HasGameReference<BounceBreaker> {
+  PlayerStick() {
+    //todo: implement
+  }
+}
