da32d53c7d3b3f43241ab6afd34a61b3fbca54db
tvlas <thiago.gilford@gmail.com>
2024-05-26 16:37:24 -0300

    feat: background color for web

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,5 @@ import 'package:flutter/material.dart';
 import 'widgets/frame.dart';
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
   runApp(const Frame());
 }
--- lib/widgets/frame.dart
+++ lib/widgets/frame.dart
@@ -44,32 +44,44 @@ class _FrameState extends State<Frame> {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Center(
-          child: Column(
-            children: [
-              Expanded(
-                child: FittedBox(
-                  fit: BoxFit.cover,
-                  child: SizedBox(
-                    width: kIsWeb ? screenWidth * 1.5 : screenWidth,
-                    height: screenHeight,
-                    child: GameWidget(
-                      game: game,
-                      overlayBuilderMap: {
-                        GameStatus.initial.name: (ctx, BounceBreaker gameRef) => InitialScreen(gameRef: gameRef),
-                        PauseButton.id: (ctx, BounceBreaker gameRef) => PauseButton(gameRef: gameRef),
-                        PauseMenu.id: (ctx, BounceBreaker gameRef) => PauseMenu(gameRef: gameRef),
-                        GameOverMenu.id: (ctx, BounceBreaker gameRef) => GameOverMenu(gameRef: gameRef),
-                        GameStatus.nextLevel.name: (ctx, BounceBreaker gameRef) => const LevelsOverlays(
-                              title: 'PROXIMA FASE',
-                              subtitle: 'TOQUE PARA CONTINUAR',
-                            ),
-                      },
+        body: Container(
+          decoration: kIsWeb
+              ? const BoxDecoration(
+                  color: Color.fromARGB(255, 41, 41, 41),
+                )
+              : const BoxDecoration(
+                  gradient: LinearGradient(
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      colors: [Colors.white, Colors.white, Color.fromARGB(255, 255, 0, 81)]),
+                ),
+          child: Center(
+            child: Column(
+              children: [
+                Expanded(
+                  child: FittedBox(
+                    fit: BoxFit.cover,
+                    child: SizedBox(
+                      width: kIsWeb ? screenWidth * 1.5 : screenWidth,
+                      height: screenHeight,
+                      child: GameWidget(
+                        game: game,
+                        overlayBuilderMap: {
+                          GameStatus.initial.name: (ctx, BounceBreaker gameRef) => InitialScreen(gameRef: gameRef),
+                          PauseButton.id: (ctx, BounceBreaker gameRef) => PauseButton(gameRef: gameRef),
+                          PauseMenu.id: (ctx, BounceBreaker gameRef) => PauseMenu(gameRef: gameRef),
+                          GameOverMenu.id: (ctx, BounceBreaker gameRef) => GameOverMenu(gameRef: gameRef),
+                          GameStatus.nextLevel.name: (ctx, BounceBreaker gameRef) => const LevelsOverlays(
+                                title: 'PROXIMA FASE',
+                                subtitle: 'TOQUE PARA CONTINUAR',
+                              ),
+                        },
+                      ),
                     ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
